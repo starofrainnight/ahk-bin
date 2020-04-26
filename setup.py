b25ad81a1c15d375be114476d153ebbcea69a5e9
Hong-She Liang <starofrainnight@gmail.com>
2020-04-26 14:53:23 +0800

    We don't provide a console program

--- setup.py
+++ setup.py
@@ -6,55 +6,50 @@
 import io
 from setuptools import setup, find_packages
 
-with io.open('README.rst', encoding='utf-8') as readme_file, io.open(
-    'HISTORY.rst', encoding='utf-8'
+with io.open("README.rst", encoding="utf-8") as readme_file, io.open(
+    "HISTORY.rst", encoding="utf-8"
 ) as history_file:
-    long_description = (readme_file.read() + "\n\n" + history_file.read())
+    long_description = readme_file.read() + "\n\n" + history_file.read()
 
 install_requires = [
-    'click>=6.0',
+    "click>=6.0",
     # TODO: put package requirements here
 ]
 
 setup_requires = [
-    'pytest-runner',
+    "pytest-runner",
     # TODO(starofrainnight): put setup requirements (distutils extensions, etc.) here
 ]
 
 tests_requires = [
-    'pytest',
+    "pytest",
     # TODO: put package test requirements here
 ]
 
 setup(
-    name='ahk-bin',
+    name="ahk-bin",
     version="0.0.1",
     description="A python package that bundled with workable AutoHotkey binary which could works for 'ahk' package ",
     long_description=long_description,
     author="Hong-She Liang",
-    author_email='starofrainnight@gmail.com',
-    url='https://github.com/starofrainnight/ahk-bin',
+    author_email="starofrainnight@gmail.com",
+    url="https://github.com/starofrainnight/ahk-bin",
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'ahk-bin=ahkbin.__main__:main'
-        ]
-    },
     include_package_data=True,
     install_requires=install_requires,
     license="Apache Software License",
     zip_safe=False,
-    keywords='ahkbin,ahk-bin',
+    keywords="ahkbin,ahk-bin",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    test_suite='tests',
+    test_suite="tests",
     tests_require=tests_requires,
     setup_requires=setup_requires,
 )
